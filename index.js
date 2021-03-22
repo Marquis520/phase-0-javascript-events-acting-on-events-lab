d0007410e6007a3b49bd5032905848bdcc536b70
Marquis520 <Marquis520@icloud.com>
2021-03-22 00:36:55 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Your code here
+// FIRST TEST
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#228B22"
+  function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+//SECOND TEST
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
+
+   
